6f71e6649461dca2b54a0745909a0491871b4ae7
Juliano Pereira <juliano340@gmail.com>
2025-06-08 20:07:34 -0300

    FEAT(frontend): adiciona label de nº de inscritos por vaga

--- frontend/src/app/pages/minhas-vagas/minhas-vagas.component.ts
+++ frontend/src/app/pages/minhas-vagas/minhas-vagas.component.ts
@@ -3,7 +3,6 @@ import { VagasService } from '../../services/vagas.service';
 import { ViewChild, ElementRef } from '@angular/core';
 import { Toast, Modal } from 'bootstrap';
 
-
 @Component({
   selector: 'app-minhas-vagas',
   templateUrl: './minhas-vagas.component.html',
@@ -13,35 +12,56 @@ export class MinhasVagasComponent implements OnInit {
   candidatos: any[] = [];
   vagaSelecionadaId: number = 0;
 
-
   @ViewChild('candidatosModal', { static: false }) modalElement!: ElementRef;
   @ViewChild('toastErro', { static: false }) toastErro!: ElementRef;
-
 
   constructor(private vagasService: VagasService) {}
 
   ngOnInit() {
     this.vagasService.minhasVagas().subscribe((res: any[]) => {
       this.vagas = res;
+      // Para cada vaga, buscar número de candidatos
+      this.vagas.forEach((vaga) => {
+        this.vagasService.listarCandidatos(vaga.id).subscribe({
+          next: (candidatos) => {
+            vaga.totalCandidatos = candidatos.length;
+          },
+          error: (err) => {
+            console.error(
+              'Erro ao buscar candidatos da vaga ID:',
+              vaga.id,
+              err
+            );
+            vaga.totalCandidatos = 0;
+          },
+        });
+      });
     });
   }
 
   excluir(id: number) {
-    if (confirm('Ao excluir esta vaga, todas as candidaturas e mensagens relacionadas também serão removidas. Deseja continuar?')) {
+    if (
+      confirm(
+        'Ao excluir esta vaga, todas as candidaturas e mensagens relacionadas também serão removidas. Deseja continuar?'
+      )
+    ) {
       this.vagasService.removerVaga(id).subscribe({
         next: () => {
-          this.vagas = this.vagas.filter(v => v.id !== id);
+          this.vagas = this.vagas.filter((v) => v.id !== id);
         },
         error: (err) => {
           console.error('Erro ao excluir vaga:', err);
-        }
+        },
       });
     }
-  }  
+  }
 
   mostrarToastErro() {
     const toastElement = this.toastErro.nativeElement;
-    const toastBootstrap = new Toast(toastElement, { delay: 3000, autohide: true });
+    const toastBootstrap = new Toast(toastElement, {
+      delay: 3000,
+      autohide: true,
+    });
     toastBootstrap.show();
   }
 
@@ -49,14 +69,22 @@ export class MinhasVagasComponent implements OnInit {
     this.vagaSelecionadaId = vagaId;
     this.vagasService.listarCandidatos(vagaId).subscribe({
       next: (data) => {
+        console.log('Candidatos recebidos:', data.length);
         this.candidatos = data;
         const modal = new Modal(document.getElementById('candidatosModal')!);
         modal.show();
       },
-      error: (err) => console.error('Erro ao buscar candidatos:', err)
+      error: (err) => console.error('Erro ao buscar candidatos:', err),
     });
   }
-  
-  
-  
+
+  numberOfCandidatos(vagaId: number) {
+    this.vagasService.listarCandidatos(vagaId).subscribe({
+      next: (data) => {
+        console.log('Número de candidatos:', data.length);
+        return Number(data.length);
+      },
+      error: (err) => console.error('Erro ao buscar candidatos:', err),
+    });
+  }
 }
